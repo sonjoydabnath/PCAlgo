7516b7629ed9129aac918b26144fb59ef0f7e63d
sonjbond <sonjoydabnath009@gmail.com>
2015-10-04 22:05:21 +0600

    LIS Updated

--- DP/LIS function.cpp
+++ DP/LIS function.cpp
@@ -15,20 +15,19 @@
 using namespace std;
 int lis( int *arr, int n )
 {
-    int *lis, i, j, max = 0;
-    lis = (int*) malloc ( sizeof( int ) * n );
+    int *lis, i, j, maxi = 1;
+    lis = ( int* ) malloc ( sizeof( int ) * n );
     for ( i = 0; i < n; i++ )
         lis[i] = 1;
     for ( i = 1; i < n; i++ )
         for ( j = 0; j < i; j++ )
-            if ( arr[i] > arr[j] && lis[i] < lis[j] + 1)
+            if ( arr[i] >= arr[j] && lis[i] < lis[j] + 1 ) {
                 lis[i] = lis[j] + 1;
-    for ( i = 0; i < n; i++ )
-        if ( max < lis[i] )
-            max = lis[i];
+                maxi = max( maxi, lis[i] );
+            }
     /* Free memory to avoid memory leak */
     free( lis );
-    return max;
+    return maxi;
 }
 
 int main()
@@ -37,3 +36,4 @@ int main()
     int n=10;
     cout<<lis(arr,n)<<endl;
 }
+
